53c949db202bdd98acc6888d9117923df91d4ca7
WilleySlippery <39125001+WilleySlippery@users.noreply.github.com>
2018-05-17 11:16:06 +0300

    Unity project added
    Fightscene. Terrain ready. Fightscene Ui started with strange bugs.

---
+++ turn/Assets/Scripts/CombatScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatScript : MonoBehaviour {
+    public GameObject CombatMenu;
+    public GameObject AttackMenu;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowAttackMenu()
+    {
+        Debug.Log("paska");
+        CombatMenu.SetActive(false);
+        AttackMenu.SetActive(true);
+    }
+}
