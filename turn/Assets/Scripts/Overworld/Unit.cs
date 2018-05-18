f984a8fd7958c5096364b1e0328e0cc60cf3b8c4
Richard Splenish <35207606+Splenish@users.noreply.github.com>
2018-05-18 12:02:04 +0300

    Pathline refining and movement started

---
+++ turn/Assets/Scripts/Overworld/Unit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	public int moveSpeed;
+
+	public List<Node> currentPath = null;
+
+	GameObject currentUnit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void MoveToNextTile() {
+		int remainingMovement = moveSpeed;
+		while (remainingMovement > 0) {
+			if (currentPath == null) {
+				return;
+			}
+			remainingMovement -= 1;
+
+			//remove the old current tile
+			currentPath.RemoveAt(0);
+			if (currentPath.Count == 1) {
+				currentPath = null;
+			}
+		}
+	}
+}
