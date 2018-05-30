a67efb5cfa86fb7e32722233d8afc50525da5bd1
WilleySlippery <t7livi00@students.oamk.fi>
2018-05-30 10:18:11 +0300

    BattleScene
    Uusi battlescene. scriptit kesken

---
+++ Turn based combat/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class BaseEnemy{
+
+    public string name;
+
+
+    public float baseHP;
+    public float curHP;
+
+    public float baseMP;
+    public float curMP;
+
+    public float baseATK;
+    public float curATK;
+    public float baseDEF;
+    public float curDEF;
+
+    public enum Type
+    {
+        Grass,
+        Fire,
+        Water,
+        Electric
+    }
+
+    public enum Rarity
+    {
+        common,
+        uncommon,
+        rare,
+        superrare
+    }
+
+    public Type EnemyType;
+    public Rarity rarity;
+
+
+
+
+	
+}
